8939e15bdd22648372dd76bc966c790e5f0e16ef
ale18aax <ale93apo@gmail.com>
2017-06-01 17:28:34 +0200

    Update turnoffall.php

--- turnoffall.php
+++ turnoffall.php
@@ -2,25 +2,20 @@
 
 $cURL = curl_init();
 
+  $url = "http://90.77.107.119:8082/api/cqk7k2oUYSRxy2yCusLuyqhnsq-wxosy3k1g0VyM/groups/1/action";
 
-// $on = $_GET['on'];
-$number = $_GET['number'];
+  $data = array('on'=>false);
+  $data_json = json_encode($data);
 
-$url = "http://90.77.107.119:8082/api/cqk7k2oUYSRxy2yCusLuyqhnsq-wxosy3k1g0VyM/lights/" . $number ."/state";
- 
-$data = array('on'=>false);
-$data_json = json_encode($data);
+  curl_setopt($cURL , CURLOPT_URL, $url);
+  curl_setopt($cURL , CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($data_json)));
+  curl_setopt($cURL , CURLOPT_CUSTOMREQUEST, 'PUT');
+  curl_setopt($cURL , CURLOPT_POSTFIELDS,$data_json);
+  curl_setopt($cURL , CURLOPT_RETURNTRANSFER, true);
 
-curl_setopt($cURL , CURLOPT_URL, $url);
-curl_setopt($cURL , CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($data_json)));
-curl_setopt($cURL , CURLOPT_CUSTOMREQUEST, 'PUT');
-curl_setopt($cURL , CURLOPT_POSTFIELDS,$data_json);
-curl_setopt($cURL , CURLOPT_RETURNTRANSFER, true);
+  $result = curl_exec($cURL);
+print $result;
 
-$result = curl_exec($cURL);
- 
-curl_close($cURL);
-  
-print_r($result);
+  curl_close($cURL);
  
 ?>
